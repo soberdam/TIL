63a70f83781c9473dbc4b85a985b525a2423159b
조담현 <j38903639@gmail.com>
2023-03-07 01:37:44 +0900

    자바 프로그래밍 실습

---
+++ JAVA/예제코드/생활코딩 JAVA1/Programming/OkJavaGoinHome.java
@@ -0,0 +1,26 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OkJavaGoinHome {
+
+	public static void main(String[] args) {
+		
+		String id = "JAVA APT 507";
+		
+		// Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		// Light on
+		Lighting floorLamp = new Lighting(id + " / floorLamp");
+		floorLamp.on();
+		
+		
+	}
+
+}
---
+++ JAVA/예제코드/생활코딩 JAVA1/Programming/OkJavaGoinHomeInput.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+import org.opentutorials.iot.DimmingLights;
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OkJavaGoinHomeInput {
+	
+	//파라미터, 매개변수
+	public static void main(String[] args) {
+		
+		String id = args[0];
+		String bright = args[1];
+		
+		// Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		// Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		// Light on
+		Lighting floorLamp = new Lighting(id + " / floorLamp");
+		floorLamp.on();
+		
+		DimmingLights moodLamp = new DimmingLights(id + " moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
+		moodLamp.on();
+		
+		
+	}
+
+}
