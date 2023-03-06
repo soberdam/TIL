86af84eb98a442ebba678b6c348c53accbbdf106
조담현 <j38903639@gmail.com>
2023-03-07 01:47:41 +0900

    Delete JAVA/예제코드/boostcourse/프로젝트A - 명함 관리 프로그램 directory

--- JAVA/예제코드/boostcourse/프로젝트A - 명함 관리 프로그램/cardmanager/ddl.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `businesscard` (
-  `name` varchar(10) DEFAULT NULL,
-  `phone` varchar(20) NOT NULL,
-  `companyName` varchar(20) DEFAULT NULL,
-  `createDate` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`phone`)
-)
