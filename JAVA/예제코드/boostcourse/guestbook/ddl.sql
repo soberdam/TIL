df2a61c231025e4244a60810ed31d89e6338ea14
조담현 <j38903639@gmail.com>
2023-03-07 01:51:19 +0900

    Delete JAVA/예제코드/boostcourse directory

--- JAVA/예제코드/boostcourse/guestbook/ddl.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `guestbook` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'guestbook id',
-  `content` TEXT NOT NULL COMMENT 'guestbook content',
-  `regdate` DATETIME NULL DEFAULT NULL COMMENT '등록일',
-  PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
