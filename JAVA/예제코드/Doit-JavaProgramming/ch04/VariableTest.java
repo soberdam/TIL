2f103a49585eb2f27e46d5914badfedadbd132ec
조담현 <j38903639@gmail.com>
2023-03-07 01:57:29 +0900

    변수 선언 및 출력 실습

---
+++ JAVA/예제코드/Doit-JavaProgramming/ch04/VariableTest.java
@@ -0,0 +1,16 @@
+package ch04;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		int age;
+		age = 10;
+		
+		int level = 1000;
+		System.out.println(age);
+		System.out.println(level);
+
+	}
+
+}
