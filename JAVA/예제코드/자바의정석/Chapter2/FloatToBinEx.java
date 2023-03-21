6aafb42d8d7ac73ba3c647303e352ab99685d1f3
조담현 <j38903639@gmail.com>
2023-03-21 15:49:41 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter2/FloatToBinEx.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+public class FloatToBinEx {
+
+	public static void main(String[] args) {
+		float f = 9.1234567f;
+		int i = Float.floatToIntBits(f);
+		
+		System.out.printf("%f%n", f);
+		System.out.printf("%X%n", i);
+	}
+
+}
