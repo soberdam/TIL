bf29b34d50a8257513b47de016aa816c3f8b37a6
조담현 <j38903639@gmail.com>
2023-03-22 23:36:24 +0900

    자바의정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter3/OperatorEx1.java
@@ -0,0 +1,15 @@
+package Chapter3;
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		int i = 5;
+		i++;
+		System.out.println(6);
+		
+		i = 5;
+		++i;
+		System.out.println(i);
+	}
+
+}
