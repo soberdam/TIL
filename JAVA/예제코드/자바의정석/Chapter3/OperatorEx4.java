a2309df95ad4120b2869fae6556a08976777f3a5
조담현 <j38903639@gmail.com>
2023-03-23 11:30:17 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter3/OperatorEx4.java
@@ -0,0 +1,15 @@
+package Chapter3;
+
+public class OperatorEx4 {
+
+	public static void main(String[] args) {
+		int i = -10;
+		i = +i;
+		System.out.println(i);
+		
+		i = -10;
+		i = -i;
+		System.out.println(i);
+	}
+
+}
