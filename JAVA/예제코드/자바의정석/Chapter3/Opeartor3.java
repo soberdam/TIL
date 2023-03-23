7fff5cb6e499079abe8b8b61f7c37ca1988a848c
조담현 <j38903639@gmail.com>
2023-03-23 11:24:20 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter3/Opeartor3.java
@@ -0,0 +1,12 @@
+package Chapter3;
+
+public class Opeartor3 {
+
+	public static void main(String[] args) {
+		int i=5, j=5;
+		System.out.println(i++);
+		System.out.println(++j);
+		System.out.println("i = " + i + ", j = " + j);
+	}
+
+}
