3933c395de8eb78dbde5a2338ebf88a671e73cc5
조담현 <j38903639@gmail.com>
2023-03-23 17:55:21 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter3/OperatorEx18.java
@@ -0,0 +1,11 @@
+package Chapter3;
+
+public class OperatorEx18 {
+
+	public static void main(String[] args) {
+		double pi = 3.141592;
+		double shortPi = Math.round(pi * 1000) / 1000.0;
+		System.out.println(shortPi);
+	}
+
+}
