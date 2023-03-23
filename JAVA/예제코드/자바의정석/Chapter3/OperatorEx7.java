1f87ff4d924cd3a85db48bfda8e71962cbb6835b
조담현 <j38903639@gmail.com>
2023-03-23 11:53:44 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter3/OperatorEx7.java
@@ -0,0 +1,12 @@
+package Chapter3;
+
+public class OperatorEx7 {
+
+	public static void main(String[] args) {
+		byte a = 10;
+		byte b = 30;
+		byte c = (byte)(a * b);
+		System.out.println(c);
+	}
+
+}
