d4d69d05d304fdae56a19bedf70f004b3fcdd2a9
조담현 <j38903639@gmail.com>
2023-03-23 14:24:51 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter3/OperatorEx15.java
@@ -0,0 +1,12 @@
+package Chapter3;
+
+public class OperatorEx15 {
+
+	public static void main(String[] args) {
+		char lowerCase = 'a';
+		char upperCase = (char)(lowerCase-32);
+		
+		System.out.println(upperCase);
+	}
+
+}
