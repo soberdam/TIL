ad1a008f055c24f99a729625a0e6b5fdafad3c6d
조담현 <j38903639@gmail.com>
2023-03-24 15:57:09 +0900

    자바의 정석 연습문제 코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter3/Exercise3_3.java
@@ -0,0 +1,10 @@
+package Chapter3;
+
+public class Exercise3_3 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(num > 0 ? "양수" : (num == 0 ? "0" : "음수"));
+	}
+
+}
