44be63c8ecc27adc8d16db261d4bfbbc7d5fbc53
조담현 <j38903639@gmail.com>
2023-03-24 16:55:15 +0900

    자바의 정석 연습문제 코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter3/Exercise3_9.java
@@ -0,0 +1,11 @@
+package Chapter3;
+
+public class Exercise3_9 {
+
+	public static void main(String[] args) {
+		char ch = 'z';
+		boolean b = (('a'<=ch&&ch<='z')||('A'<=ch&&ch<='Z')||('0'<=ch&&ch<='9'));
+		System.out.println(b);
+	}
+
+}
