fd68aa0b847bafa0054f6e790ff4397020b68c0e
조담현 <j38903639@gmail.com>
2023-03-29 16:40:11 +0900

    자바의 정석 연습문제 코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter4/Exercise4_9.java
@@ -0,0 +1,13 @@
+package Chapter4;
+
+public class Exercise4_9 {
+
+	public static void main(String[] args) {
+		String str = "12345";
+		int sum = 0;
+		for (int i = 0; i < str.length(); i++) {
+			sum += str.charAt(i) - '0';
+		}
+		System.out.println("sum=" + sum);
+	}
+}
