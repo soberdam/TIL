51d2ec7e168c7b5923e9e4dcc3c0567721b648a4
조담현 <j38903639@gmail.com>
2023-03-29 16:29:32 +0900

    자바의 정석 연습문제 코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter4/Exercise4_5.java
@@ -0,0 +1,22 @@
+package Chapter4;
+
+public class Exercise4_5 {
+
+	public static void main(String[] args) {
+//		for(int i=0; i<=10; i++) {
+//		for(int j=0; j<=i; j++)
+//			System.out.print("*");
+//		System.out.println();
+//	}
+		int i = 0;
+		while (i <= 10) {
+			int j = 0;
+			while (j <= i) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
