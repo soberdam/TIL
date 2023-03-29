1969d58d51a6a48267f35c7124221695dff93cc8
조담현 <j38903639@gmail.com>
2023-03-29 16:21:30 +0900

    자바의 정석 연습문제 코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter4/Exercise4_1.java
@@ -0,0 +1,28 @@
+//		다음의 문장들을 조건식으로 표현하라.
+//		1. int형 변수 x가 10보다 크고 20보다 작을 때 true인 조건식
+//		2. char형 변수 ch가 공백이나 탭이 아닐 때 true인 조건식
+//		3. char형 변수 ch가 ‘x' 또는 ’X'일 때 true인 조건식
+//		4. char형 변수 ch가 숫자(‘0’~‘9’)일 때 true인 조건식
+//		5. char형 변수 ch가 영문자(대문자 또는 소문자)일 때 true인 조건식
+//		6. int형 변수 year가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로 나눠떨어지지
+//		않을 때 true인 조건식
+//		7. boolean형 변수 powerOn가 false일 때 true인 조건식
+//		8. 문자열 참조변수 str이 “yes”일 때 true인 조건식
+package Chapter4;
+public class Exercise4_1 {
+
+	public static void main(String[] args) {
+
+		
+//		System.out.println(x > 10 && x < 20);
+//		ch != ' ' && ch != '\t'
+//		ch == x || ch == 'X'
+//		ch >= '0' && ch <= '9'
+//		(ch>='a' && ch<='z') || (ch>='A' && ch <='Z')
+//		year%400 == 0 || (year % 4 == 0 && year%100 !=0)
+//		!powerOn
+//		str.equals("yes")
+
+	}
+
+}
