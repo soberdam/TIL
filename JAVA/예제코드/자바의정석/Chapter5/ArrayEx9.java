17949610fc5955f462b9342b6fdd42e637873b32
조담현 <j38903639@gmail.com>
2023-03-30 16:09:00 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter5/ArrayEx9.java
@@ -0,0 +1,19 @@
+package Chapter5;
+
+import java.util.Arrays;
+
+public class ArrayEx9 {
+
+	public static void main(String[] args) {
+		int[] code = {-4, -1, 3, 6, 11};
+		int[] arr = new int [10];
+		
+		for(int i=0; i<arr.length; i++) {
+			int tmp = (int)(Math.random()*5);
+			arr[i] = code[tmp];
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
