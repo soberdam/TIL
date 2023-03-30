db07d07d51f31f50fe93810ac3983aab59e43624
조담현 <j38903639@gmail.com>
2023-03-30 16:34:00 +0900

    자바의 정석 예제코드 업로드

--- JAVA/예제코드/자바의정석/Chapter5/ArrayEx10.java
+++ JAVA/예제코드/자바의정석/Chapter5/ArrayEx10.java
@@ -10,22 +10,22 @@ public class ArrayEx10 {
 		}
 		
 		System.out.println();
-		
-		for(int i=0; i<numArr.length-1; i++) {
+		//버블정렬
+		for(int i=0; i<numArr.length-1; i++) { // n-1번 반복해서 비교한다
 			boolean changed = false; // 자리바꿈 발생했는지 체크
 			
-			for(int j=0; j<numArr.length-1-i; j++) { // 버블정렬
+			for(int j=0; j<numArr.length-1-i; j++) { // 마지막 원소를 정렬 후 비교에서 제외하므로 비교횟수가 1씩 줄어든다
 				if(numArr[j] > numArr[j+1]) {
 					int temp = numArr[j];
 					numArr[j] = numArr[j+1];
 					numArr[j+1] = temp;
-					changed = true;
+					changed = true; 
 				}
 			}
-			if(!changed) break;
+			if(!changed) break; // 자리바꿈이 발생하지 않으면 정렬 완료, 반복문을 벗어난다.
 			
 			for(int k=0; k<numArr.length; k++) {
-				System.out.print(numArr[k]);
+				System.out.print(numArr[k]); // 정렬될때마다(1회전마다) 출력
 			}
 			System.out.println();
 		}
