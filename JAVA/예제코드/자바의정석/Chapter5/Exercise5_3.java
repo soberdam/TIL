f11fddecaa57c7f9ab5ad27df5c08f817754fa1b
조담현 <j38903639@gmail.com>
2023-03-31 10:22:32 +0900

    자바의 정석 연습문제 코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter5/Exercise5_3.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+public class Exercise5_3 {
+
+	public static void main(String[] args) {
+		int[] arr = {10, 20, 30, 40, 50};
+		int sum = 0;
+		for (int i=0; i<arr.length; i++) {
+			sum+=arr[i];
+		}
+		System.out.println("sum="+sum);
+	}
+
+}
