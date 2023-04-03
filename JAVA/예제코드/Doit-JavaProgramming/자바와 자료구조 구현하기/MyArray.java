57063bd3633d46e95321df2ff6f6806c16416246
조담현 <j38903639@gmail.com>
2023-04-03 23:40:48 +0900

    자바로 배열 구현하기 실습코드 업로드

---
+++ JAVA/예제코드/Doit-JavaProgramming/자바와 자료구조 구현하기/MyArray.java
@@ -0,0 +1,104 @@
+package ch02;
+
+public class MyArray {
+	int[] intArr;
+	int count; // 개수
+	
+	public int ARRAY_SIZE;
+	public static final int ERROR_NUM = -99999999;
+	
+	public MyArray() {
+		count = 0;
+		ARRAY_SIZE = 10;
+		intArr = new int[ARRAY_SIZE];
+		
+	}
+	
+	public MyArray(int size) {
+		count = 0;
+		ARRAY_SIZE = size;
+		intArr = new int[size];
+	}
+	
+	public int getSize() {
+		return count;
+	}
+	
+	public boolean isEmpty() {
+		if(count == 0) return true;
+		
+		return false;
+	}
+	
+	public void addElement(int num) {
+		if(count>=ARRAY_SIZE) {
+			System.out.println("not enough memoty");
+			return;
+		}
+		intArr[count++] = num;
+	}
+	
+	public void insertElement(int position, int num) {
+		if(count>=ARRAY_SIZE) {
+			System.out.println("not enough memoty");
+			return;
+		}
+		
+		if(position <0 || position > count) {
+			System.out.println("insert error");
+			return;
+		}
+		
+		for(int i = count-1; i>=position; i--) {
+			intArr[i+1] = intArr[i];
+		}
+		intArr[position] = num;
+		count++;
+	}
+	
+	public int removeElement(int position) {
+		if(isEmpty()) {
+			System.out.println("There is no element");
+			return ERROR_NUM;
+		}
+		
+		if(position <0 || position >= count) {
+			System.out.println("remove error");
+			return ERROR_NUM;
+		}
+		int result = intArr[position];
+		for(int i = position; i<=count-1; i++) {
+			intArr[i] = intArr[i+1];
+		}
+		count--;
+	
+		return result;
+	}
+	
+	public int getElement(int position) {
+		if(position <0 || position >= count) {
+			System.out.println("검색 위치 오류. 현재 리스트의 개수는 \" + count +\"개 입니다.");
+			return ERROR_NUM;
+		}
+		return intArr[position];
+	}
+	
+	public void printAll() {
+		if(isEmpty()) {
+			System.out.println("출력할 내용이 없습니다");
+			return;
+		}
+		
+		for(int i=0; i<count; i++) {
+			System.out.println(intArr[i]);
+		}
+	}
+	
+	public void removeAll() {
+		for(int i=0; i<count; i++) {
+			intArr[i] = 0;
+		}
+		count = 0;
+	}
+}
+	
