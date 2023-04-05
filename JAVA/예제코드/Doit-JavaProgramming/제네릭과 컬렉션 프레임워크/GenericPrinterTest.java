e7edc55fce5bc44c841a630657fed65293369ae2
조담현 <j38903639@gmail.com>
2023-04-05 12:13:43 +0900

    제네릭 타입 사용하기 실습코드 업로드

---
+++ JAVA/예제코드/Doit-JavaProgramming/제네릭과 컬렉션 프레임워크/GenericPrinterTest.java
@@ -0,0 +1,15 @@
+package ch01;
+
+public class GenericPrinterTest {
+
+	public static void main(String[] args) {
+		GenericPrinter<Powder> powderPrinter = new GenericPrinter<>();
+		powderPrinter.setMaterial(new Powder());
+		System.out.println(powderPrinter);
+		
+		GenericPrinter<Plastic> plasticPrinter = new GenericPrinter<>();
+		plasticPrinter.setMaterial(new Plastic());
+		System.out.println(plasticPrinter);
+	}
+
+}
