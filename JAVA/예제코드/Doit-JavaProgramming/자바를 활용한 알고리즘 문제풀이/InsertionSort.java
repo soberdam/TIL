2f8371ae4c5a3ee51f201876f3f226c815063953
조담현 <j38903639@gmail.com>
2023-04-13 17:13:33 +0900

    삽입정렬 구현해보기 실습코드 업로드

---
+++ JAVA/예제코드/Doit-JavaProgramming/자바를 활용한 알고리즘 문제풀이/InsertionSort.java
@@ -0,0 +1,37 @@
+
+public class InsertionSort {
+	
+	public static void insertionSort(int[] arr, int count) {
+		int i = 0; 
+		int j = 0;
+		int temp = 0;
+		
+		for(i=1; i<count; i++) {
+			temp = arr[i];
+			j = i;
+			while(j>0 && arr[j-1]>temp) {
+				arr[j] = arr[j-1];
+				j=j-1;
+			}
+			arr[j] = temp;
+			
+			System.out.println("반복 - " + i);
+			printSort(arr, count);
+		}
+	}
+	public static void printSort(int[] arr, int count) {
+		for(int i=0; i<count; i++) {
+			System.out.print(arr[i] + "\t");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		
+		int[] arr = {80, 50, 70, 10, 60, 20, 40, 30 };
+		
+		insertionSort(arr, arr.length);
+
+		
+	}
+
+}
