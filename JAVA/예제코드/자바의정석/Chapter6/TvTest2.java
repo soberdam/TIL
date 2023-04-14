ebe393feda631284942dc4f58c98c6df7e3dd0eb
조담현 <j38903639@gmail.com>
2023-04-14 15:50:19 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter6/TvTest2.java
@@ -0,0 +1,21 @@
+package Chapter6;
+class Tv{
+	String color;
+	boolean power;
+	int channel;
+	
+	void power() { power = !power; };
+	void channelUp() {++channel;}
+	void channelDown() {--channel;}
+}
+public class TvTest2 {
+
+	public static void main(String[] args) {
+		Tv t;
+		t = new Tv();
+		t.channel = 7;
+		t.channelDown();
+		System.out.println("현재 채널은 " + t.channel + "입니다.");
+	}
+
+}
