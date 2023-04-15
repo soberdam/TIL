5fc5ac1aa3a9db25ca6e57fe5884dc5b878dae82
조담현 <j38903639@gmail.com>
2023-04-15 16:19:57 +0900

    미로 찾기 문제 실습코드 업로드

---
+++ JAVA/예제코드/Doit-JavaProgramming/자바를 활용한 알고리즘 문제풀이/MazeTest.java
@@ -0,0 +1,17 @@
+package ch06;
+
+public class MazeTest {
+
+	public static void main(String[] args) {
+
+		Robot robot;
+		System.out.println("출구는 입니다. (7,7)");
+
+		robot = new Robot();
+
+		robot.findPath(0, 0, 7, 7);
+		robot.showPath();
+
+	}
+
+}
---
+++ JAVA/예제코드/Doit-JavaProgramming/자바를 활용한 알고리즘 문제풀이/Move.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class Move {
+	int direction = 0;
+	
+	public int x = 0;
+	public int y = 0;
+	
+	public Move(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
