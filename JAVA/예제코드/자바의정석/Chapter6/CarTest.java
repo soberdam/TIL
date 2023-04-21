d1762737445c48dcb9f3f323ec66a3f54d70d86a
조담현 <j38903639@gmail.com>
2023-04-21 15:39:18 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter6/CarTest.java
@@ -0,0 +1,33 @@
+package Chapter6;
+
+class Car {
+	String color;
+	String gearType;
+	int door;
+	
+	
+	Car() {}
+	
+	Car(String c, String g, int d) {
+		this.color = c;
+		this.gearType = g;
+		this.door = d;
+	}
+}
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		c1.color = "white";
+		c1.gearType = "auto";
+		c1.door = 4;
+		
+		Car c2 = new Car("white", "auto", 4);
+		
+		System.out.println("c1의 color=" + c1.color + ", gearType=" + c1.gearType + ", door=" + c1.door);
+		System.out.println("c2의 color=" + c2.color + ", gearType=" + c2.gearType + ", door=" + c2.door);
+				
+	}
+
+}
