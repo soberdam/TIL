1f7c29df8ae6d7439d8320b988ad94bf8394365f
조담현 <j38903639@gmail.com>
2023-05-07 14:58:51 +0900

    자바의 정석 연습문제 코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter6/Exercise6_24.java
@@ -0,0 +1,13 @@
+package Chapter6;
+
+class Exercise6_24 {
+	static int abs(int value) {
+		return value < 0 ? -value : value;
+	}
+	public static void main(String[] args) {
+		int value = 5;
+		System.out.println(value + "의 절대값:" + abs(value));
+		value = -10;
+		System.out.println(value + "의 절대값:" + abs(value));
+	}
+}
