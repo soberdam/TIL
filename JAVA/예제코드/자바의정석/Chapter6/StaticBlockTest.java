4ff510f79415a8ba2edddd2ca329ed31fb76a0d7
조담현 <j38903639@gmail.com>
2023-07-24 14:18:17 +0900

    자바의 정석 예제코드 업로드

--- JAVA/예제코드/자바의정석/Chapter6/StaticBlockTest.java
+++ JAVA/예제코드/자바의정석/Chapter6/StaticBlockTest.java
@@ -1,18 +1,18 @@
-package Chapter6;
+package 정석;
 
 public class StaticBlockTest {
 	static int[] arr = new int [10];
 	
 	static {
 		for(int i=0; i<arr.length; i++) {
-			arr[i] = (int)(Math.random()*10) + 1;
+			arr[i] = (int)(Math.random()*10)+1;
 		}
 	}
-	
 	public static void main(String[] args) {
-		for(int i=0; i<arr.length; i++){
-			System.out.println("arr["+i+"] : " + arr[i]);
+		for(int i=0; i<arr.length; i++) {
+			System.out.println("arr[" + i +"] :" + arr[i]);
 		}
+
 	}
 
 }
