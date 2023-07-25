2a96fe25d3ed6574ff27cb2c85cfacac4a4fd18d
조담현 <j38903639@gmail.com>
2023-07-25 10:33:04 +0900

    자바의 정석 예제코드 업로드

---
+++ JAVA/예제코드/자바의정석/Chapter7/StaticImportTest.java
@@ -0,0 +1,14 @@
+package ch7;
+import static java.lang.System.out;
+import static java.lang.Math.*;
+public class StaticImportTest {
+
+	public static void main(String[] args) {
+//		System.out.println(Math.random());
+		out.println(random());
+		
+//		System.out.println("Math. PI : " + Math.PI);
+		out.println(PI);
+	}
+
+}
