39c9cb4664c3b80d343121fa456e116bed0e8952
조담현 <j38903639@gmail.com>
2023-08-16 17:07:19 +0900

    Update ExceptionEx11.java

--- JAVA/예제코드/자바의정석/Chapter8/ExceptionEx11.java
+++ JAVA/예제코드/자바의정석/Chapter8/ExceptionEx11.java
@@ -1,5 +1,5 @@
 class ExceptionEx11 {
 	public static void main(String[] args) {
-		throw new RuntimeException();	// RuntimeExceptionÀ» °íÀÇ·Î ¹ß»ý½ÃÅ²´Ù.
+		throw new RuntimeException();	// RuntimeExceptionÃ€Â» Â°Ã­Ã€Ã‡Â·ÃŽ Â¹ÃŸÂ»Ã½Â½ÃƒÃ…Â²Â´Ã™.
 	}
 }
